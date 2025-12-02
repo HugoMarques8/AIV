b56774787c472026192e399f8fbbf61cda56b535
Debs <151763151+panadinho@users.noreply.github.com>
2025-12-02 17:27:15 +0000

    corrected something with the hand

--- js/handedness-setup.js
+++ js/handedness-setup.js
@@ -36,21 +36,22 @@ AFRAME.registerComponent('vr-hands', {
     // Configurar funcionalidades por mão dominante
     if (dominantHand === 'right') {
       // Destro: mão direita agarra, mão esquerda tem laser
+      rightHand.setAttribute('oculus-touch-controls', 'hand: right; model: false');
       rightHand.setAttribute('hand-controls', 'hand: right; handModelStyle: lowPoly');
       rightHand.setAttribute('hand-tracking-grab-controls', 'hand: right');
       rightHand.setAttribute('raycaster', 'objects: .grabbable; far: 0.1');
       
-      leftHand.setAttribute('oculus-touch-controls', 'hand: left');
+      leftHand.setAttribute('oculus-touch-controls', 'hand: left; model: false');
       leftHand.setAttribute('laser-controls', 'hand: left; model: true');
       leftHand.setAttribute('raycaster', 'objects: .clickable, .grabbable');
     } else {
       // Esquerdino: mão esquerda agarra, mão direita tem laser
-      leftHand.setAttribute('oculus-touch-controls', 'hand: left');
+      leftHand.setAttribute('oculus-touch-controls', 'hand: left; model: false');
       leftHand.setAttribute('hand-controls', 'hand: left; handModelStyle: lowPoly');
       leftHand.setAttribute('hand-tracking-grab-controls', 'hand: left');
       leftHand.setAttribute('raycaster', 'objects: .grabbable; far: 0.1');
       
-      rightHand.setAttribute('oculus-touch-controls', 'hand: right');
+      rightHand.setAttribute('oculus-touch-controls', 'hand: right; model: false');
       rightHand.setAttribute('laser-controls', 'hand: right; model: true');
       rightHand.setAttribute('raycaster', 'objects: .clickable, .grabbable');
     }
