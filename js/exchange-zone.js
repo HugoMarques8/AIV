3a66818b638bd61889a923cf4f868a7d5fde3ef6
Debs <151763151+panadinho@users.noreply.github.com>
2025-12-28 23:26:05 +0000

    Added a cool info to look for the challenge keepers

--- js/exchange-zone.js
+++ js/exchange-zone.js
@@ -1,11 +1,11 @@
 /* global AFRAME, THREE */
 
 AFRAME.registerComponent('exchange-zone', {
-  init: function() {
+  init: function () {
     this.swordPlaced = false;
     this.ringPlaced = false;
     this.checkInterval = null;
-    
+
     // Criar texto de debug
     /* 
     this.debugText = document.createElement('a-text');
@@ -16,65 +16,65 @@ AFRAME.registerComponent('exchange-zone', {
     this.debugText.setAttribute('width', '4');
     this.el.sceneEl.appendChild(this.debugText);
     */
-    
+
     // Começar a verificar objetos a cada 500ms
     this.checkInterval = setInterval(() => {
       this.checkObjects();
     }, 500);
   },
 
-  checkObjects: function() {
+  checkObjects: function () {
     const zonePos = new THREE.Vector3();
     this.el.object3D.getWorldPosition(zonePos);
     const zoneRadius = 1.5; // Raio do cilindro
-    
+
     // Verificar espada
     const sword = document.getElementById('player-sword');
     if (sword && !this.swordPlaced) {
       const swordPos = new THREE.Vector3();
       sword.object3D.getWorldPosition(swordPos);
       const distance = Math.sqrt(
-        Math.pow(swordPos.x - zonePos.x, 2) + 
+        Math.pow(swordPos.x - zonePos.x, 2) +
         Math.pow(swordPos.z - zonePos.z, 2)
       );
-      
+
       if (distance < zoneRadius) {
         this.placeSword(sword);
       }
     }
-    
+
     // Verificar anel
     const ring = document.getElementById('ritual-ring');
     if (ring && !this.ringPlaced) {
       const ringPos = new THREE.Vector3();
       ring.object3D.getWorldPosition(ringPos);
       const distance = Math.sqrt(
-        Math.pow(ringPos.x - zonePos.x, 2) + 
+        Math.pow(ringPos.x - zonePos.x, 2) +
         Math.pow(ringPos.z - zonePos.z, 2)
       );
-      
+
       if (distance < zoneRadius) {
         this.placeRing(ring);
       }
     }
-    
+
     // Atualizar debug
     /*
     const status = 'Sword: ' + (this.swordPlaced ? 'YES' : 'NO') + ' | Ring: ' + (this.ringPlaced ? 'YES' : 'NO');
     this.debugText.setAttribute('value', status);
     */
-    
+
     // Se ambos colocados, completar ritual
     if (this.swordPlaced && this.ringPlaced) {
       this.completeRitual();
     }
   },
 
-  placeSword: function(sword) {
+  placeSword: function (sword) {
     this.swordPlaced = true;
     sword.setAttribute('visible', 'false');
     console.log('Sword placed in exchange zone!');
-    
+
     // Efeito visual
     const flash = document.createElement('a-sphere');
     flash.setAttribute('position', '2 0.5 -6');
@@ -85,17 +85,17 @@ AFRAME.registerComponent('exchange-zone', {
     flash.setAttribute('animation', 'property: scale; to: 6 6 6; dur: 2000; easing: easeOutQuad');
     flash.setAttribute('animation__fade', 'property: material.opacity; to: 0; dur: 2000; easing: easeOutQuad');
     this.el.sceneEl.appendChild(flash);
-    
+
     setTimeout(() => {
       flash.parentNode.removeChild(flash);
     }, 2000);
   },
 
-  placeRing: function(ring) {
+  placeRing: function (ring) {
     this.ringPlaced = true;
     ring.setAttribute('visible', 'false');
     console.log('Ring placed in exchange zone!');
-    
+
     // Efeito visual
     const flash = document.createElement('a-sphere');
     flash.setAttribute('position', '2 0.5 -6');
@@ -106,22 +106,22 @@ AFRAME.registerComponent('exchange-zone', {
     flash.setAttribute('animation', 'property: scale; to: 6 6 6; dur: 2000; easing: easeOutQuad');
     flash.setAttribute('animation__fade', 'property: material.opacity; to: 0; dur: 2000; easing: easeOutQuad');
     this.el.sceneEl.appendChild(flash);
-    
+
     setTimeout(() => {
       flash.parentNode.removeChild(flash);
     }, 2000);
   },
 
-  completeRitual: function() {
+  completeRitual: function () {
     clearInterval(this.checkInterval);
-    
+
     /*
     this.debugText.setAttribute('value', 'RITUAL COMPLETE!');
     this.debugText.setAttribute('color', '#00FF00');
     */
-    
+
     console.log('Ritual completed! Showing portal...');
-    
+
     // Mostrar portal de volta
     setTimeout(() => {
       const portal = document.getElementById('return-portal');
@@ -131,7 +131,7 @@ AFRAME.registerComponent('exchange-zone', {
     }, 2000);
   },
 
-  remove: function() {
+  remove: function () {
     if (this.checkInterval) {
       clearInterval(this.checkInterval);
     }
